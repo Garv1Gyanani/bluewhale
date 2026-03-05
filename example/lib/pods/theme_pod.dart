3322149f9bd989162a1e3225490fe2a10fb3f0ca
Garv <garvgyanani101@gmail.com>
2026-03-05 11:58:10 +0530

    example: add theme pod for dark mode

---
+++ example/lib/pods/theme_pod.dart
@@ -0,0 +1,6 @@
+import 'package:blue_whale/blue_whale.dart';
+
+// true for dark mode, false for light mode
+class ThemePod extends WhalePod<bool> {
+  ThemePod() : super(false); // Default to light mode
+}
